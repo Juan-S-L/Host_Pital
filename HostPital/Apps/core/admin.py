066037f9ea0a49d27aca4fd43ce5f0c8ba06f574
Juan Sebastian <jsebastianlu16@gmail.com>
2023-04-17 11:30:41 -0500

    Se formateo la base de datos por un 'poqueñ' erroro

--- HostPital/Apps/core/admin.py
+++ HostPital/Apps/core/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
-from Apps.core.models import User
+from Apps.core.models import User, DoctorEsp
 # Register your models here.
-
-# admin.site.register(User)
 
 @admin.register(User)
 class User(admin.ModelAdmin):
     list_display = ('id','tipo','username','first_name','last_name','email','cc','is_active')
     list_filter = ('tipo','is_active',)
     search_fields = ('id','tipo','first_name','last_name','cc')
+    
+@admin.register(DoctorEsp)
+class DocEsp(admin.ModelAdmin):
+    list_display = ('id','doctor','espcialidad')
+    list_filter = ('espcialidad',)
+    search_fields = ('id', 'espcialidad')
--- HostPital/Apps/core/models.py
+++ HostPital/Apps/core/models.py
@@ -12,3 +12,41 @@ class User(AbstractUser):
     telefono = models.PositiveIntegerField(help_text=('+ 57'), null=True, blank=True)
     fechaNacimiento = models.DateField(help_text=('AAAA-MM-DD'),null=True, blank=True,verbose_name='Fecha nacimiento')
     tipo = models.CharField(max_length=58, null=True, blank=True, choices=tipoUsuario,verbose_name='Tipo de usuario')
+    
+class DoctorEsp(models.Model):
+    TIPOS_DE_MEDICOS = (
+        ('anestesiologo', 'Anestesiólogo'),
+        ('cardiologo', 'Cardiólogo'),
+        ('cirujano', 'Cirujano'),
+        ('dermatologo', 'Dermatólogo'),
+        ('endocrinologo', 'Endocrinólogo'),
+        ('gastroenterologo', 'Gastroenterólogo'),
+        ('ginecologo', 'Ginecólogo'),
+        ('hematologo', 'Hematólogo'),
+        ('infectologo', 'Infectólogo'),
+        ('internista', 'Internista'),
+        ('nefrologo', 'Nefrólogo'),
+        ('neurologo', 'Neurólogo'),
+        ('oftalmologo', 'Oftalmólogo'),
+        ('oncologo', 'Oncólogo'),
+        ('otorrinolaringologo', 'Otorrinolaringólogo'),
+        ('patologo', 'Patólogo'),
+        ('pediatra', 'Pediatra'),
+        ('psiquiatra', 'Psiquiatra'),
+        ('radiologo', 'Radiólogo'),
+        ('reumatologo', 'Reumatólogo'),
+        ('traumatologo', 'Traumatólogo'),
+        ('general', 'Médico General'),
+    )
+    user_doctor = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Doctor',null=True, blank=True,limit_choices_to={'tipo':'doctor'})
+    espcialidad = models.CharField(max_length=68, choices=TIPOS_DE_MEDICOS, verbose_name='Especialidad',null=True, blank=True)
+    
+    @property
+    def doctor(self):
+        return f'{self.user_doctor.first_name} {self.user_doctor.last_name}'
+    
+    class Meta():
+        verbose_name = 'Dr. con especialidad'
+        verbose_name_plural = 'Dr. con especialidades'
+
+    
