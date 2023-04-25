2506ca49af20650c2262c2a2ab3334f8d7c043e5
Juan Sebastian <jsebastianlu16@gmail.com>
2023-04-25 16:46:57 -0500

    Se crea formulario para la informacion del cliente en perfil

--- HostPital/Apps/medios_internos/forms.py
+++ HostPital/Apps/medios_internos/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Cita
+from .models import Cita, InfoCliente
 from Apps.core.models import User
 
 class CitaForm(forms.ModelForm):
@@ -8,3 +8,12 @@ class CitaForm(forms.ModelForm):
     class Meta:
         model = Cita
         fields = ['user_doctor','hora',]
+        
+class InfoCliente(forms.ModelForm):
+    sexo = forms.ChoiceField(label='Genero', choices=InfoCliente.tipoSexo, initial='M')
+    direccionResidencia = forms.CharField(label='Direccion de residencia', max_length=80)
+    nacionalidad = forms.CharField(label='Nacionalidad', max_length=80)
+    
+    class Meta:
+        model = InfoCliente
+        fields = ['sexo','direccionResidencia','nacionalidad']
