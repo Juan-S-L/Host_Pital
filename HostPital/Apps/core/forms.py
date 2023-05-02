eacc3a2834cd8a1c46cf56bbe68e0b4d40574c23
Juan Sebastian <jsebastianlu16@gmail.com>
2023-05-02 14:36:10 -0500

    Se le implemento al formulario de regitro la opcion de genero, direccion de residencia

--- HostPital/Apps/core/forms.py
+++ HostPital/Apps/core/forms.py
@@ -3,11 +3,21 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class RegistroUsuario(UserCreationForm, forms.ModelForm):
+    SEXO_CHOICES = (
+        ('M', 'Masculino'),
+        ('F', 'Femenino'),
+        ('O', 'Otro')
+    )
+    
     cc = forms.IntegerField(label='Numero de documento')
     email = forms.EmailField(label='Correo')
     telefono = forms.IntegerField(label='Numero de telefono')
     fechaNacimiento = forms.DateField(label='Fecha de nacimiento', help_text='AAAA-MM-DD')
     tipo = forms.CharField(initial='cliente')
+    
+    sexo = forms.ChoiceField(label='Genero', choices=SEXO_CHOICES, initial='M')
+    direccionResidencia = forms.CharField(label='Direccion de residencia', max_length=80)
+    
     password1 = forms.CharField(label='Contraseña',widget=forms.PasswordInput)
     password2 = forms.CharField(label='Confirma contraseña',widget=forms.PasswordInput)
     
@@ -18,5 +28,5 @@ class RegistroUsuario(UserCreationForm, forms.ModelForm):
     
     class Meta:
         model = User
-        fields = ['first_name','last_name','cc','email','telefono','fechaNacimiento','password1','password2']
+        fields = ['first_name','last_name','cc','email','telefono','fechaNacimiento','sexo','direccionResidencia','password1','password2']
         help_text = {k:"" for k in fields}
