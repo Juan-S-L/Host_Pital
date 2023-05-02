cb435e9e79598c9a7852e8867a70a83a9c5c916d
Juan Sebastian <jsebastianlu16@gmail.com>
2023-05-02 14:38:08 -0500

    Se implemento dos URL para el doctor y para el cliente

--- HostPital/Apps/core/urls.py
+++ HostPital/Apps/core/urls.py
@@ -6,11 +6,13 @@ urlpatterns = [
     path('registro/',views.registro, name='registro'),
     path('inicio/',views.index, name='inicio'),
     path('logout/',views.cerrarSesion, name='logout'),
-    path('inicio/perfil/',views.perfil, name='perfil'),
     path('login/',views.inicioSesion, name='login'),
     
+    path('inicio/perfil/',views.perfil, name='perfil'),
     path('inicio/citas/',views.cita, name='cita'),
     path('inicio/solicitarCita/',views.solicitarCita, name='solicitarCita'),
-    
+    path('inicio/pacientes/',views.pacientes, name='pacientes'),
+    path('inicio/pacientes/historialclinico/<int:id>',views.historialClinico, name='historialClinico'), 
+    path('inicio/historialclinico/',views.historialClinico_Cliente, name="historialClinico_cliente")
     
 ]
