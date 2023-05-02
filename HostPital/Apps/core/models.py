f61e8e1c7ad137f572eac322bae7333f70123652
Juan Sebastian <jsebastianlu16@gmail.com>
2023-05-02 14:37:15 -0500

    Se implemento el tipo de sexo para utilizarlo en el formulario de registro y direccion de residencia

--- HostPital/Apps/core/models.py
+++ HostPital/Apps/core/models.py
@@ -8,7 +8,17 @@ class User(AbstractUser):
         ('enfermero', 'Enfermero'),
         ('cliente', 'Cliente'),
     )
+    tipoSexo = (
+        
+        ('M','Masculino'),
+        ('F','Femenino'),
+        ('O','Otro'),
+    )
+    
     cc = models.IntegerField(null=True, blank=True,verbose_name='Numero de documento')
     telefono = models.PositiveIntegerField(help_text=('+ 57'), null=True, blank=True)
     fechaNacimiento = models.DateField(help_text=('AAAA-MM-DD'),null=True, blank=True,verbose_name='Fecha nacimiento')
     tipo = models.CharField(max_length=58, null=True, blank=True, choices=tipoUsuario,verbose_name='Tipo de usuario')
+
+    sexo = models.CharField(max_length=68, choices=tipoSexo, verbose_name='Genero',null=True, blank=True)
+    direccionResidencia = models.CharField(max_length=100, null=True, blank=True, verbose_name='Direccion de residencia')
