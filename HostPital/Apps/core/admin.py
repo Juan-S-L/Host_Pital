3fedc959949defb60e393b9a159e03c91b6ecd37
Juan Sebastian <jsebastianlu16@gmail.com>
2023-05-02 14:34:25 -0500

    No se han hecho modificaciones

--- HostPital/Apps/core/admin.py
+++ HostPital/Apps/core/admin.py
@@ -4,7 +4,7 @@ from Apps.core.models import User
 
 @admin.register(User)
 class User(admin.ModelAdmin):
-    list_display = ('id','tipo','username','first_name','last_name','email','cc','is_active')
+    list_display = ('id','tipo','sexo','username','first_name','last_name','email','cc','is_active')
     list_filter = ('tipo','is_active',)
     search_fields = ('id','tipo','first_name','last_name','cc')
     
