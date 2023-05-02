6cba05557085d9a836213960969b2e36694f8218
Juan Sebastian <jsebastianlu16@gmail.com>
2023-05-02 14:44:48 -0500

    Se agrego un decorador del modelo HistorialCLinete para el administrador

--- HostPital/Apps/medios_internos/admin.py
+++ HostPital/Apps/medios_internos/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from Apps.medios_internos.models import DoctorEsp, Cita
+from Apps.medios_internos.models import DoctorEsp, Cita, HistorialCliente
 # Register your models here.
 
 @admin.register(DoctorEsp)
@@ -12,3 +12,8 @@ class DocEsp(admin.ModelAdmin):
 class cita_(admin.ModelAdmin):
     list_display = ('id','doctor','cliente','fechaHora_I','fechaHora_F')
     readonly_fields = ('fechaHora_F',)
+    
+@admin.register(HistorialCliente)
+class HistorialClinico(admin.ModelAdmin):
+    list_display = ('id','cliente','doctor','fecha_actualizacion')
+    readonly_fields = ('fecha_actualizacion','fecha_creacion',)
